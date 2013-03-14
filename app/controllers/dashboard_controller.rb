e6df918016b6112b12614d7aaa661f38007814a0
Will Ricketts <will@willricketts.com>
2013-03-13 21:40:06 -0400

    Added stolen item count and list to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,7 @@ class DashboardController < ApplicationController
   def index
     @user = current_user
     @items = current_user.items.all
+    @stolenitems = current_user.items.find(:all, :conditions => {:stolen => true})
   end
   
 end
