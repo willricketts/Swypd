726f96e2a282e931bb9a89fd16dade0b4629b711
Will Ricketts <will@willricketts.com>
2013-03-16 17:39:41 -0400

    Added a terrible excuse for 'my account'

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,26 @@
 class AccountsController < ApplicationController
   before_filter :authenticate_user!
   def index
-    
+    @user = current_user
+    render :template => 'devise/passwords/edit', :locals => {:resource => current_user,:resource_name => User }
+  end
+  
+  def show
+    @user = current_user
+    render '../views/devise/registrations/edit'
+  end
+  
+  def edit
+    @user = current_user
+    render '../views/devise/registrations/edit'
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to :controller => "dashboard", :action => "index", :notice => "Your account was updated."
+    else
+      render "edit"
+    end
   end
 end
